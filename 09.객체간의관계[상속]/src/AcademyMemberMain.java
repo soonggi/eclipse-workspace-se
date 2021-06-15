aa58194a36a710144b915d8579400b066792d63c
황순근 <@DESKTOP-3MS5Q0P>
2021-06-15 14:18:38 +0900

    ..

---
+++ 09.객체간의관계[상속]/src/AcademyMemberMain.java
@@ -0,0 +1,34 @@
+
+public class AcademyMemberMain {
+
+	public static void main(String[] args) {
+		AcademyStudent st1 = new AcademyStudent(1, "KIM", "LINUX");
+		AcademyStudent st2 = new AcademyStudent(2, "WIM", "JAVA");
+		AcademyStudent st3 = new AcademyStudent(3, "EIM", "DESIGN");
+		
+		AcademyGangsa gs1 = new AcademyGangsa(4, "LEE", "ANDROID");
+		AcademyGangsa gs2 = new AcademyGangsa(5, "ZEE", "IOS");
+		
+		AcademyStaff sf1 = new AcademyStaff(6, "RRL", "MARKETING");
+		AcademyStaff sf2 = new AcademyStaff(7, "JEONG", "ACCOUNTING");
+		
+		/*
+		st1.print();
+		st2.print();
+		st3.print();
+		 */
+		
+		System.out.println("--------------Student----------------");
+		st1.studentPrint();
+		st2.studentPrint();
+		st3.studentPrint();
+		System.out.println("--------------Gangsa----------------");
+		gs1.gangsaPrint();
+		gs2.gangsaPrint();
+		System.out.println("--------------Staff----------------");
+		sf1.staffPrint();
+		sf2.staffPrint();
+
+	}
+
+}
---
+++ 09.객체간의관계[상속]/src/AcademyStudent.java
@@ -0,0 +1,52 @@
+public class AcademyStudent extends AcademyMember {
+	/*
+	public int no;
+	public String name;
+	public AcademyMember() {
+		
+	}
+	public AcademyMember(int no, String name) {
+		super();
+		this.no = no;
+		this.name = name;
+	}
+	public void print() {
+		System.out.print(no+"\t"+name+"\t");
+	}
+	
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	 */
+	
+	public String ban; // 반
+	public AcademyStudent() {
+		
+	}
+	public AcademyStudent(int no, String name, String ban) {
+		this.no=no;
+		this.name=name;
+		this.ban=ban;
+	}
+	public String getBan() {
+		return ban;
+	}
+	public void setBan(String ban) {
+		this.ban = ban;
+	}
+	public void studentPrint() {
+		this.print();
+		System.out.println(this.ban);
+	}
+	
+
+}
